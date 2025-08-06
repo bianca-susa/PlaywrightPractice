ec9b24bc717f1b00b2dfbc01804ab072db9c21e1
bianca-susa <biancasusa.23@gmail.com>
2025-08-06 21:32:37 +0300

    Assignment 3 - Locators

--- src/main/java/assignment/PlaywrightTest.java
+++ src/main/java/assignment/PlaywrightTest.java
@@ -1,6 +1,7 @@
 package assignment;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
 
 public class PlaywrightTest {
 
@@ -15,6 +16,29 @@ public class PlaywrightTest {
             System.out.println("3. Current URL: " + page.url());
 
             Locator logo = page.locator("#header > div > a > img.large");
+            logo.click();
+
+            page.navigate("http://qa3magento.dev.evozon.com/women.html");
+
+            page.goBack();
+            page.goForward();
+            page.reload();
+
+            page.close();
+        }
+    }
+
+    public static void homepageUpdated()
+    {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false) );
+            Page page = browser.newPage();
+            page.navigate("http://qa3magento.dev.evozon.com/");
+            System.out.println("2. Page title: " + page.title());
+            System.out.println("3. Current URL: " + page.url());
+
+            Locator logo = page.locator("a.logo");
             logo.click();
 
             page.navigate("http://qa3magento.dev.evozon.com/women.html");
@@ -45,6 +69,22 @@ public class PlaywrightTest {
         }
     }
 
+    public static void accountUpdated()
+    {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false) );
+            Page page = browser.newPage();
+            page.navigate("http://qa3magento.dev.evozon.com/");
+
+
+            Locator accountButton = page.locator("a[data-target-element=\"#header-account\"]");
+            accountButton.click();
+
+            page.close();
+        }
+    }
+
     public static void search()
     {
         try (Playwright playwright = Playwright.create()) {
@@ -64,6 +104,25 @@ public class PlaywrightTest {
         }
     }
 
+    public static void searchUpdated()
+    {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false) );
+            Page page = browser.newPage();
+            page.navigate("http://qa3magento.dev.evozon.com/");
+
+            Locator searchField = page.locator("#search");
+            searchField.clear();
+            searchField.fill("woman");
+
+            Locator submitButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Search"));
+            submitButton.click();
+
+            page.close();
+        }
+    }
+
     public static void newProductsList()
     {
         try (Playwright playwright = Playwright.create()) {
@@ -73,6 +132,27 @@ public class PlaywrightTest {
             page.navigate("http://qa3magento.dev.evozon.com/");
 
             Locator newProducts = page.locator("//*[@id=\"top\"]/body/div/div/div[2]/div/div/div[2]/div[2]/div[2]/ul/li");
+            int productCount = newProducts.count();
+            System.out.println("Number of products: " + productCount);
+
+            for(Locator product: newProducts.all())
+            {
+                System.out.println("Product: " + product);
+            }
+
+            page.close();
+        }
+    }
+
+    public static void newProductsListUpdated()
+    {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false) );
+            Page page = browser.newPage();
+            page.navigate("http://qa3magento.dev.evozon.com/");
+
+            Locator newProducts = page.locator("ul.products-grid.products-grid--max-5-col-widget > li");
             int productCount = newProducts.count();
             System.out.println("Number of products: " + productCount);
 
@@ -106,6 +186,27 @@ public class PlaywrightTest {
         }
     }
 
+    public static void navigationUpdated()
+    {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false) );
+            Page page = browser.newPage();
+            page.navigate("http://qa3magento.dev.evozon.com/");
+
+            Locator headlines = page.locator(".nav-primary");
+            for(Locator headline: headlines.all())
+            {
+                System.out.println(headline.innerText());
+            }
+
+            Locator sale = page.getByText("Sale", new Page.GetByTextOptions().setExact(true));
+            sale.click();
+
+            page.close();
+        }
+    }
+
     public static void addProductToCart()
     {
         try (Playwright playwright = Playwright.create()) {
@@ -133,6 +234,39 @@ public class PlaywrightTest {
             quantityField.fill("2");
 
             Locator addToCartButton = page.locator("#product_addtocart_form > div.product-shop > div.product-options-bottom > div.add-to-cart > div.add-to-cart-buttons > button");
+            addToCartButton.click();
+
+            page.close();
+        }
+    }
+
+    public static void addProductToCartUpdated()
+    {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false) );
+            Page page = browser.newPage();
+            page.navigate("http://qa3magento.dev.evozon.com/");
+
+            Locator category = page.locator(".nav-primary> *:first-child");
+            category.click();
+
+            Locator subCategory = page.locator(".nav-primary> *:first-child > ul > *:nth-child(3)");
+            subCategory.click();
+
+            Locator product = page.locator("ul.products-grid.products-grid--max-4-col.first.last.odd>li:first-of-type");
+            product.click();
+
+            Locator colorOption = page.locator("#swatch21 > span.swatch-label");
+            colorOption.click();
+
+            Locator sizeOption = page.locator("#swatch80 > span.swatch-label");
+            sizeOption.click();
+
+            Locator quantityField = page.locator("#qty");
+            quantityField.fill("2");
+
+            Locator addToCartButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add to cart"));
             addToCartButton.click();
 
             page.close();
@@ -192,13 +326,70 @@ public class PlaywrightTest {
         }
     }
 
+    public static void removeProductFromCartUpdated()
+    {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(
+                    new BrowserType.LaunchOptions().setHeadless(false) );
+            Page page = browser.newPage();
+            page.navigate("http://qa3magento.dev.evozon.com/");
+
+            Locator category = page.locator(".nav-primary> *:first-child");
+            category.click();
+
+            Locator subCategory = page.locator(".nav-primary> *:first-child > ul > *:nth-child(3)");
+            subCategory.click();
+
+            Locator product1 = page.locator("ul.products-grid.products-grid--max-4-col.first.last.odd>li:first-of-type");
+            product1.click();
+
+            Locator colorOption = page.locator("#swatch21 > span.swatch-label");
+            colorOption.click();
+
+            Locator sizeOption = page.locator("#swatch80 > span.swatch-label");
+            sizeOption.click();
+
+            Locator quantityField = page.locator("#qty");
+            quantityField.fill("2");
+
+            Locator addToCartButton = page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Add to cart"));
+            addToCartButton.click();
+
+            page.goBack();
+            page.goBack();
+
+            Locator product2 = page.locator("ul.products-grid.products-grid--max-4-col.first.last.odd>li:nth-child(3)");
+            product2.click();
+
+            colorOption.click();
+            sizeOption.click();
+
+            quantityField.fill("3");
+
+            addToCartButton.click();
+
+            Locator removeFromCartButton = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("Remove item")).first();
+            removeFromCartButton.click();
+
+            page.close();
+        }
+    }
+
     public static void main(String[] args) {
-        homepage();
-        account();
-        search();
-        newProductsList();
-        navigation();
-        addProductToCart();
-        removeProductFromCart();
+//        homepage();
+//        account();
+//        search();
+//        newProductsList();
+//        navigation();
+//        addProductToCart();
+//        removeProductFromCart();
+
+        homepageUpdated();
+        accountUpdated();
+        searchUpdated();
+        newProductsListUpdated();
+        navigationUpdated();
+        addProductToCartUpdated();
+        removeProductFromCartUpdated();
     }
 }
